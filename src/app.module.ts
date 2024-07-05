a3d9664609c371e31aea9f28c121ec72d287d41a
Andrey Lantsov <andreyonohin88@gmail.com>
2024-07-05 18:20:58 +0300

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersService } from './offers/offers.service';
+import { OffersController } from './offers/offers.controller';
+import { OffersModule } from './offers/offers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [],
+  imports: [UsersModule, WishesModule, WishlistsModule, OffersModule, TypeOrmModule.forRoot()],
+  controllers: [AppController, OffersController],
+  providers: [OffersService],
 })
 export class AppModule {}
